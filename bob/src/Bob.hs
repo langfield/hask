34fa04d48f799e7adf39473c2e381387698fc234
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-08-25 09:45:13 -0500

    Update `Bob.hs`.

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,6 +1,5 @@
 module Bob (responseFor) where
 
-import Debug.Trace (trace)
 import Data.Char (isUpper, isAlpha, isSpace)
 
 isQuestion :: String -> Bool
@@ -10,19 +9,17 @@ isQuestion s  = last s == '?'
 isYell :: String -> Bool
 isYell text = any isAlpha text && all isUpper (filter isAlpha text)
 
-responseForQuestion :: String -> String
-responseForQuestion question
-  | isYell question = "Calm down, I know what I'm doing!"
-  | otherwise = "Sure."
-
+strip :: String -> String
+strip = f . f
+  where f = reverse . dropWhile isSpace
 
 responseFor :: String -> String
-responseFor text = responseForStripped $ filter isSpace (trace ("Input: " ++ text) text)
-
+responseFor text = responseForStripped $ strip text
 
 responseForStripped :: String -> String
 responseForStripped text
-  | dropWhile isSpace text == "" = "Fine. Be that way!"
-  | isQuestion text = responseForQuestion text
+  | text == "" = "Fine. Be that way!"
+  | isQuestion text && isYell text = "Calm down, I know what I'm doing!"
+  | isQuestion text = "Sure."
   | isYell text = "Whoa, chill out!"
   | otherwise = "Whatever."
