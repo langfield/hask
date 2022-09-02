89d6e91cd906a9034c85e3ca4d7b044bfde80a3b
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-09-02 11:43:56 -0400

    Update docstring.

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -3,17 +3,15 @@ module DNA (toRNA) where
 type RNAChar = Char
 type InvalidChar = Char
 
--- For nonempty strings, validate the first character.
---  * If it's invalid, unwrap and then immediately rewrap with `Left`.
---  * If it's vaiid, recurse and prepend the validated character to the result
---    in the case where the result of the recursion is a ``Right String``.
+-- Translate all the characters in the string, and then combine the ``Either``s
+-- into a single ``Either``.
 toRNA :: String -> Either Char String
 toRNA s = combine $ map translate s
 
--- Basically a clone of `Control.Monad.sequence`.
+-- Basically a clone of ``Control.Monad.sequence``.
 combine :: [Either a b] -> Either a [b]
 combine []             = Right []
-combine (Left c : _)  = Left c
+combine (Left c : _)   = Left c
 combine (Right c : cs) = either Left (\xs -> Right (c : xs)) (combine cs)
 
 -- Validate and lift a character.
