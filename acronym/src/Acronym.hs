194f093076cdd5a4ef334325dc5a677189e0dda5
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-09-19 11:42:45 -0500

    Add more.

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,48 +2,48 @@ module Acronym
   ( abbreviate
   ) where
 
-import Data.Char (isLower, isUpper, toUpper)
+import Data.Char (isUpper, toUpper)
 
 -- I figured I'd do this with ``String`` first, and then write one with ``Data.Text``.
 abbreviate :: String -> String
-abbreviate = upperHeads . concat . allHumps . words . normalize
+abbreviate = concatMap abbreviateToken . words . normalize
   where
-    upperHeads = map $ toUpper . head
-    allHumps = map dehump
-    removeBadPunc = filter (not . isBadPunc)
     normalize = map delimsToSpaces . removeBadPunc
 
--- Check if a character is superfluous punctuation.
-isBadPunc :: Char -> Bool
-isBadPunc '\'' = True
-isBadPunc '_' = True
-isBadPunc _ = False
+-- Remove superfluous punctuation from a string.
+removeBadPunc :: String -> String
+removeBadPunc = filter (not . isBadPunc)
+  where isBadPunc c = c `elem` ['\'', '_']
 
 -- Convert delimiters to spaces.
 delimsToSpaces :: Char -> Char
-delimsToSpaces '-' = ' '
-delimsToSpaces ',' = ' '
-delimsToSpaces c = c
+delimsToSpaces c = if c `elem` ['-', ','] then ' ' else c
 
--- Split a token into a list of PascalCase humps.
-dehump :: String -> [String]
-dehump [] = []
-dehump (c:cs)
-  | allUpper (c : cs) = [[c]]
-  | isLower c = (c : lower) : dehump rest
-  | otherwise = upperHump (c : cs)
-  where
-    allUpper = all isUpper
-    lower = takeWhile isLower cs
-    rest = dropWhile isLower cs
+-- Comments
+-- ========
+-- If the word is all uppercase, we need only the first letter.
+-- Otherwise, we need all uppercase letters.
+--
+-- Do the tests properly disambiguate what we should do for the case
+-- `GNUImage`? Should this be `GNUI` or `GI`?
+--
+-- I think it should be `GI`.
+--
+-- What about `aGNUImage`? Should that be `AGI` or `GNUI` or `AGNUI`?
+--
+-- I think it should be `AGI`.
 
--- Handle case where first letter is uppercase.
-upperHump :: String -> [String]
-upperHump [] = [""]
-upperHump (c:"") = [[c]]
-upperHump (c:d:ds)
-  | isUpper d = [c] : dehump (d : ds)
-  | otherwise = (c : lower) : dehump rest
-  where
-    lower = takeWhile isLower (d : ds)
-    rest = dropWhile isLower (d : ds)
+-- As an aside, do we put spaces around cons, no spaces around cons, or does it
+-- change depending on the case?
+--
+-- Spaces it is!
+--
+
+-- Abbreviate a single token, converting result to uppercase.
+abbreviateToken :: String -> [Char]
+abbreviateToken [] = []
+abbreviateToken (c : cs)
+  | allUpper (c : cs) || allLower (c : cs) = [toUpper c]
+  | otherwise = filter isUpper (c : cs)
+  where allUpper = all isUpper
+        allLower = not . any isUpper
