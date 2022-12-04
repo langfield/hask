6b0e907f8ddb58a94eaefba56625c4598e44d95e
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-12-03 23:57:51 -0500

    Finish red-black tree delete function.

--- custom-set/src/CustomSet.hs
+++ custom-set/src/CustomSet.hs
@@ -1,4 +1,4 @@
-module Tree
+module CustomSet
   ( delete
   , difference
   , empty
@@ -36,21 +36,50 @@ del x t@(T _ a y b)
  
 -- Delete `x` from the left child of a tree.
 delL :: Ord a => a -> Tree a -> Tree a
+-- If the left child is black, delete from it and make the tree black, then
+-- rebalance.
 delL x (T _ a@(T B _ _ _) y b) = balL $ T B (del x a) y b
+-- Otherwise, delete from it and make the tree red.
 delL x (T _ a y b) = T R (del x a) y b
 
+-- Rebalance a black tree after we've deleted from a black left subtree.
 balL :: Tree a -> Tree a
+-- If the subtree is now red, swap colors.
 balL (T B (T R a x b) y c) = T R (T B a x b) y c
+-- Otherwise, if the right subtree is black, make it red and balance.
+balL (T B a x (T B b y c)) = balance' (T B a x (T R b y c))
+-- Otherwise, if the right subtree is red, we go fucking mental.
+balL (T B a x (T R (T B b y c) z d@(T B e w f))) = T R (T B a x b) y (balance' (T B c z (T R e w f)))
 
 -- Delete `x` from the right child of a tree.
 delR :: Ord a => a -> Tree a -> Tree a
 delR x (T _ a y b@(T B _ _ _)) = balR $ T B a y (del x b)
 delR x (T _ a y b) = T R a y (del x b)
 
+-- Rebalance a black tree after we've deleted from a black right subtree.
 balR :: Tree a -> Tree a
-balR (T B (T R a x b) y c) = T R (T B a x b) y c
+-- If right subtree is red, swap colors with root.
+balR (T B a x (T R b y c)) = (T R a x (T B b y c))
+-- Otherwise, if left subtree is black, change it to red and balance.
+balR (T B (T B a x b) y c) = balance' (T B (T R a x b) y c)
+-- Otherwise, if left subtree is red, go crazy.
+balR (T B (T R (T B a x b) y (T B c z d)) w e) = T R (balance' (T B (T R a x b) y c)) z (T B d w e)
 
-fuse = error ""
+-- When we actually find `x` in `del`, it is the root of some tree, and we must
+-- remove it and `fuse` its subtrees into a single replacement.
+fuse :: Tree a -> Tree a -> Tree a
+fuse E t = t
+fuse t E = t
+fuse a@(T B _ _ _) (T R b x c) = T R (fuse a b) x c
+fuse (T R a x b) c@(T B _ _ _) = T R a x (fuse b c)
+fuse (T R a x b) (T R c y d) =
+  case fuse b c of
+    (T R s' z' t') -> (T R (T R a x s') z' (T R t' y d))
+    bc@(T B _ _ _) -> (T R a x (T R bc y d))
+fuse (T B a x b) (T B c y d) =
+  case fuse b c of
+    (T R i' z' j') -> (T R (T B a x i') z' (T B j' y d))
+    bc@(T B _ _ _) -> balL (T B a x (T B bc y d))
 
 difference :: Ord a => Tree a -> Tree a -> Tree a
 difference setA setB = foldr (\b set -> delete b set) setA $ toList setB
@@ -116,3 +145,6 @@ balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
 balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
 balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
 balance color a x b = T color a x b
+
+balance' :: Tree a -> Tree a
+balance' (T color a x b) = balance color a x b
