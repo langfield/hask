018773cfc43e9cda614ef9d1b57f589905210fbf
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-12-22 07:23:25 -0500

    Restructure types in poker problem.

--- poker/src/Poker.hs
+++ poker/src/Poker.hs
@@ -4,6 +4,7 @@ module Poker (bestHands) where
 
 import Control.Applicative ((<|>))
 import Data.Attoparsec.Text (Parser)
+import Data.Map (Map)
 import GHC.Data.Maybe (rightToMaybe)
 
 import qualified Data.Attoparsec.Text as Atto
@@ -12,23 +13,32 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
 
+type High = Card
+type Quad = Card
+type Kicker = Card
+type HighKicker = Card
+type MiddleKicker = Card
+type LowKicker = Card
+type Triplet = Card
+type Pair = Card
+type HighPair = Card
+type LowPair = Card
+
 data Category
-    = StraightFlush
-    | FourOfAKind
-    | FullHouse
+    = StraightFlush High
+    | FourOfAKind Quad Kicker
+    | FullHouse Triplet Pair
     | Flush
-    | Straight
-    | ThreeOfAKind
-    | TwoPair
-    | OnePair
-    | HighCard deriving Eq
+    | Straight High
+    | ThreeOfAKind Triplet HighKicker LowKicker
+    | TwoPair HighPair LowPair Kicker
+    | OnePair Pair HighKicker MiddleKicker LowKicker
+    | HighCard
+    deriving (Eq)
 data Rank = Number Integer | Jack | Queen | King | Ace deriving (Eq)
 data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq)
 data Card = Card Rank Suit
-data Hand = Hand Category (Card, Card, Card, Card, Card)
-
-getCardList :: (Card, Card, Card, Card, Card) -> [Card]
-getCardList (c1, c2, c3, c4, c5) = [c1, c2, c3, c4, c5]
+data Hand = Hand Category [Card]
 
 instance Show Rank where
   show (Number k) = show k
@@ -50,7 +60,7 @@ instance Show Card where
 -- to high cards, we lose the ordering of the hand, which means our output will
 -- be wrong.
 instance Show Hand where
-  show (Hand _ cards) = (unwords . map show . getCardList) cards
+  show (Hand _ cards) = (unwords . map show) cards
 
 instance Eq Card where
   (==) (Card a _) (Card b _) = a == b
@@ -59,25 +69,71 @@ instance Ord Card where
   (<=) a b = scoreCardHigh a <= scoreCardHigh b
 
 instance Eq Hand where
-  (Hand cat1 (a1, _, _, _, _)) == (Hand cat2 (a2, _, _, _, _)) =
-    cat1 == cat2 && a1 == a2
+  (Hand (StraightFlush h) _) == (Hand (StraightFlush h') _) = h == h'
+  (Hand (FourOfAKind q k) _) == (Hand (FourOfAKind q' k') _) =
+    q == q' && k == k'
+  (Hand (FullHouse t p) _) == (Hand (FullHouse t' p') _) = t == t' && p == p'
+  (Hand Flush cs) == (Hand Flush cs') = L.sort cs == L.sort cs'
+  (Hand (Straight h) _) == (Hand (Straight h') _) = h == h'
+  (Hand (ThreeOfAKind t h l) _) == (Hand (ThreeOfAKind t' h' l') _) =
+    t == t' && h == h' && l == l'
+  (Hand (TwoPair h l k) _) == (Hand (TwoPair h' l' k') _) =
+    h == h' && l == l' && k == k'
+  (Hand (OnePair p h m l) _) == (Hand (OnePair p' h' m' l') _) =
+    p == p' && h == h' && m == m' && l == l'
+  (Hand HighCard cs) == (Hand HighCard cs') = L.sort cs == L.sort cs'
+  _ == _ = False
 
 instance Ord Hand where
-  (Hand StraightFlush a) <= (Hand StraightFlush b) = a <= b
-  (Hand StraightFlush _) <= _ = False
-  (Hand FourOfAKind a) <= (Hand FourOfAKind b) = a <= b
-  (Hand FourOfAKind _) <= _ = False
-  (Hand FullHouse a) <= (Hand FullHouse b) = a <= b
-  (Hand FullHouse _) <= _ = False
+  (Hand (StraightFlush h) _) <= (Hand (StraightFlush h') _) = h <= h'
+  (Hand (StraightFlush _) _) <= _ = False
+  (Hand (FourOfAKind q k) _) <= (Hand (FourOfAKind q' k') _)
+    | q == q' && k <= k' = True
+    | q < q' = True
+    | otherwise = False
+  (Hand (FourOfAKind _ _) _) <= _ = False
+  (Hand (FullHouse t p) _) <= (Hand (FullHouse t' p') _)
+    | t == t' && p <= p' = True
+    | t < t' = True
+    | otherwise = False
+  (Hand (FullHouse _ _) _) <= _ = False
+  (Hand Flush cs) <= (Hand Flush cs') = (reverse . L.sort) cs <= (reverse . L.sort) cs'
+  (Hand Flush _) <= _ = False
+  (Hand (Straight h) _) <= (Hand (Straight h') _) = h <= h'
+  (Hand (Straight _) _) <= _ = False
+  (Hand (ThreeOfAKind t h l) _) <= (Hand (ThreeOfAKind t' h' l') _)
+    | t < t' = True
+    | t == t' && h < h' = True
+    | t == t' && h == h' && l <= l' = True
+    | otherwise = False
+  (Hand (ThreeOfAKind _ _ _) _) <= _ = False
+  (Hand (TwoPair h l k) _) <= (Hand (TwoPair h' l' k') _)
+    | h < h' = True
+    | h == h' && l < l' = True
+    | h == h' && l == l' && k <= k' = True
+    | otherwise = False
+  (Hand (TwoPair _ _ _) _) <= _ = False
+  (Hand (OnePair p h m l) _) <= (Hand (OnePair p' h' m' l') _)
+    | p < p' = True
+    | p == p' && h < h' = True
+    | p == p' && h == h' && m < m' = True
+    | p == p' && h == h' && m == m' && l <= l' = True
+    | otherwise = False
+  (Hand (OnePair _ _ _ _) _) <= _ = False
+  (Hand HighCard cs) <= (Hand HighCard cs') = (reverse . L.sort) cs <= (reverse . L.sort) cs'
+  _ <= _ = False
+  
 
 bestHands :: [String] -> Maybe [String]
-bestHands xs = map show . best <$> maybeHands
+bestHands xs = 
+  case eitherHands of
+    Left msg -> Just [msg]
+    Right hands -> (Just . map show . best) hands
   where
-    maybeHands = (rightToMaybe . mapM (Atto.parseOnly handParser . T.pack)) xs
-
+    eitherHands = (mapM (Atto.parseOnly handParser . T.pack)) xs
 
 best :: [Hand] -> [Hand]
-best hs = case L.sort hs of
+best hs = case (reverse . L.sort) hs of
   [] -> []
   (i : is) -> i : takeWhile (== i) is
 
@@ -95,29 +151,32 @@ scoreCardLow c = scoreCardHigh c
 handParser :: Parser Hand
 handParser = do
   cs <- Atto.count 5 cardParser
-  if isUnique cs
-    then case cs of
-      [c1, c2, c3, c4, c5] -> handParser' (c1, c2, c3, c4, c5)
-    else fail "Hand has duplicate cards"
+  if isUnique cs then handParser' cs else fail "Hand has duplicate cards"
   where
     isUnique :: Ord a => [a] -> Bool
     isUnique xs = length xs == (length . S.fromList) xs
 
-handParser' :: (Card, Card, Card, Card, Card) -> Parser Hand
-handParser' cards
-  | isStraight cs && isFlush cs = pure $ Hand StraightFlush cards
-  | L.maximum counts == 4 = pure $ Hand FourOfAKind cards
-  | take 2 counts == [3, 2] = pure $ Hand FullHouse cards
-  | isFlush cs    = pure $ Hand Flush cards
-  | isStraight cs = pure $ Hand Straight cards
-  | L.maximum counts == 3 = pure $ Hand ThreeOfAKind cards
-  | take 2 counts == [2, 2] = pure $ Hand TwoPair cards
-  | take 1 counts == [2] = pure $ Hand OnePair cards
-  | otherwise     = pure $ Hand HighCard cards
+handParser' :: [Card] -> Parser Hand
+handParser' cs
+  | length cs /= 5 = fail "Hand does not have 5 cards"
+  | otherwise = case (isStraight cs, isFlush cs, countAssocs) of
+    (True, True, _) -> pure $ Hand (StraightFlush high) cs
+    (_, _, (4, [q]) : (1, [k]) : _) -> pure $ Hand (FourOfAKind q k) cs
+    (_, _, (3, [t]) : (2, [p]) : _) -> pure $ Hand (FullHouse t p) cs
+    (_, True, _) -> pure $ Hand Flush cs
+    (True, _, _) -> pure $ Hand (Straight (L.maximum cs)) cs
+    (_, _, (3, [t]) : (1, [l, h]) : _) -> pure $ Hand (ThreeOfAKind t h l) cs
+    (_, _, (2, [l, h]) : (1, [k]) : _) -> pure $ Hand (TwoPair h l k) cs
+    (_, _, (2, [p]) : (1, [l, m, h]) : _) -> pure $ Hand (OnePair p h m l) cs
+    _ -> pure $ Hand HighCard cs
   where
-    cs     = getCardList cards
-    counts = (L.reverse . L.sort . M.elems . M.fromListWith (+))
+    high = L.maximum cs
+    countAssocs = (M.toDescList . M.map L.sort . invert . M.fromListWith (+))
       (zip cs (repeat (1 :: Integer)))
+
+invert :: Ord v => Map k v -> Map v [k]
+invert m = M.fromListWith (++) pairs
+  where pairs = [ (v, [k]) | (k, v) <- M.toList m ]
 
 isSame :: (Eq a) => [a] -> Bool
 isSame [] = True
