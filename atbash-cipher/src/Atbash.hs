82700ab25a520622e17b0bd1b123d39c17de9b13
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-01-06 20:35:21 -0500

    Reformat

--- atbash-cipher/src/Atbash.hs
+++ atbash-cipher/src/Atbash.hs
@@ -4,12 +4,12 @@ import qualified Data.Char as C
 
 invert :: String -> String
 invert "" = ""
-invert (c:cs) = go c : invert cs
+invert (c : cs) = go c : invert cs
   where
     go :: Char -> Char
     go d
       | C.isAlpha d = C.chr (25 - (C.ord d - C.ord 'a') + C.ord 'a')
-      | otherwise = d
+      | otherwise   = d
 
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
@@ -19,4 +19,6 @@ decode :: String -> String
 decode s = invert $ concat $ words s
 
 encode :: String -> String
-encode s = unwords $ chunksOf 5 $ invert $ map C.toLower $ filter (`notElem` [' ', ',', '.']) s
+encode s = unwords $ chunksOf 5 $ invert $ map C.toLower $ filter
+  (`notElem` [' ', ',', '.'])
+  s
