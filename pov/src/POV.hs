3a79305731c30cf09d7a958ee7b35e9dc878e653
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-02-15 21:46:24 -0500

    Add more POV

--- pov/src/POV.hs
+++ pov/src/POV.hs
@@ -1,13 +1,21 @@
 module POV (fromPOV, tracePathBetween) where
 
-import Data.Tree (Tree)
+import Data.Maybe (mapMaybe, listToMaybe)
+import Data.Tree (Tree (..))
 
 -- | Given a tree, return a tree with the root at the given node.
 --
 -- We do this by finding the node `x` and then flipping the direction of every
 -- edge from `x` to the root.
 fromPOV :: Eq a => a -> Tree a -> Maybe (Tree a)
-fromPOV x tree = 
+fromPOV x tree = Nothing
+  where
+    root' = search 
+
+search :: Eq a => a -> Tree a -> Maybe (Tree a)
+search x root@(Node y children)
+  | x == y = Just root
+  | otherwise = listToMaybe $ mapMaybe (search x) children
 
 tracePathBetween :: Eq a => a -> a -> Tree a -> Maybe [a]
 tracePathBetween from to tree = error "You need to implement this function."
