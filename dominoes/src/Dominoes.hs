b4691fc835150f51e233f67ecc34eaa6b52af5a9
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-03-09 20:21:48 -0500

    Finally came up with some reasonable data structures

--- dominoes/src/Dominoes.hs
+++ dominoes/src/Dominoes.hs
@@ -2,17 +2,26 @@ module Dominoes (chain) where
 
 import qualified Data.Maybe as MB
 
-data Zipper = Zipper [(Int, Int)] (Int, Int) [(Int, Int)]
+type Domino = (Int, Int)
+data Zipper = Zipper [Domino] Domino [Domino]
+data State = State Int [Domino]
 
 -- | Compute a zipper traversal that shifts items on the left to the right.
 travel :: Zipper -> [Zipper]
 travel z@(Zipper [] _ _) = [z]
 travel z@(Zipper (l : ls) x rs) = z : travel (Zipper ls l (x : rs))
 
-search :: Zipper -> Maybe [(Int, Int)]
+-- | Entrypoint to the search procedure, which recall is searching for a chain of dominoes.
+search :: Zipper -> Maybe [Domino]
 search (Zipper [] d []) = Just [d]
-search (Zipper (x : xs) d []) = 
+search (Zipper (l : ls) (a, b) []) = 
+search (Zipper [] d (r : rs)) = Nothing
+search (Zipper (l : ls) d (r : rs)) = Nothing
 
-chain :: [(Int, Int)] -> Maybe [(Int, Int)]
+searchWithStart :: Zipper -> Maybe [Domino]
+
+-- | Recursive searching function, to which we give our starting point, and the value
+
+chain :: [Domino] -> Maybe [Domino]
 chain [] = Just []
 chain (d : ds) = (MB.listToMaybe . MB.mapMaybe search . travel) (Zipper ds d [])
