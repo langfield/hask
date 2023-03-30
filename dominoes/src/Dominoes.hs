69d7f306be02e48f7885018c42ee581c53afa290
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-03-29 20:31:08 -0400

    Add someone else's better solution to dominoes problem

--- dominoes/src/Dominoes.hs
+++ dominoes/src/Dominoes.hs
@@ -1,49 +1,21 @@
-module Dominoes (chain, isCyclic, search, Domino) where
+module Dominoes (chain) where
 
+import Control.Monad (msum)
+import Data.List (delete)
 import Data.Tuple (swap)
 
-import qualified Data.List as L
-import qualified Data.Maybe as MB
+chain :: [(Int, Int)] -> Maybe [(Int, Int)]
+chain [] = Just []
+chain (x@(l, r) : xs) = (x :) <$> chain' l r xs
 
-import Debug.Trace (trace, traceM)
-
-trace' :: Show a => String -> a -> a
-trace' s x = trace (s ++ ": " ++ show x) x
-
-type Domino = (Int, Int)
-
-chain :: [Domino] -> Maybe [Domino]
-chain = L.find isCyclic . search 0
-
-isCyclic :: [Domino] -> Bool
-isCyclic [] = True
-isCyclic [(a, b)] = a == b
-isCyclic ((a, _) : ds) = a == (snd . last $ ds)
-
-prepend :: Domino -> [Domino] -> Maybe [Domino]
-prepend (a, b) [] = Just [(a, b)]
-prepend (a, b) ((a', b') : ds)
-  | b == a'   = Just $ (a, b) : (a', b') : ds
-  | otherwise = Nothing
-
-search' :: Int -> [Domino] -> Domino -> [[Domino]]
-search' _ [] _ = []
-search' depth ds (a, b) = forwardResults ++ backwardResults
+-- | Compute a domino chain to which we can prepend `(l, r)`.
+chain' :: Int -> Int -> [(Int, Int)] -> Maybe [(Int, Int)]
+chain' l r [] = if l == r then Just [] else Nothing
+chain' l r xs = msum $ map try xs
   where
-    forwardResults = MB.mapMaybe (prepend (a, b)) . search (depth + 1) . delete' (a, b) $ ds
-    backwardResults = MB.mapMaybe (prepend (b, a)) . search (depth + 1) . delete' (b, a) $ ds
-
-search :: Int -> [Domino] -> [[Domino]]
-search _ [] = [[]]
-search depth ds = concatMap (search' depth ds) ds
-
--- Delete a swapped domino if no non-swapped version is found.
-delete' :: Domino -> [Domino] -> [Domino]
-delete' _ [] = []
-delete' (a, b) ds@(_ : tl)
-  | length ds' < length ds = ds'
-  | length ds'' < length ds = ds''
-  | otherwise = tl
-  where
-    ds'  = L.delete (a, b) ds
-    ds'' = L.delete (b, a) ds
+    -- Try using `x` as the next domino (in both orientations).
+    try :: (Int, Int) -> Maybe [(Int, Int)]
+    try x@(l', r')
+      | l' == r   = (x :) <$> chain' l r' (delete x xs)
+      | r' == r   = (swap x :) <$> chain' l l' (delete x xs)
+      | otherwise = Nothing
