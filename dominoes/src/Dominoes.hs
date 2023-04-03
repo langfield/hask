4a953c1f8abeaed59bea9dfee2f7c89f72335c39
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-02 20:06:35 -0400

    Add a working solution that I wrote myself!

--- dominoes/src/Dominoes.hs
+++ dominoes/src/Dominoes.hs
@@ -4,18 +4,22 @@ import Control.Monad (msum)
 import Data.List (delete)
 import Data.Tuple (swap)
 
-chain :: [(Int, Int)] -> Maybe [(Int, Int)]
-chain [] = Just []
-chain (x@(l, r) : xs) = (x :) <$> chain' l r xs
+import qualified Data.Maybe as MB
 
--- | Compute a domino chain to which we can prepend `(l, r)`.
+chain :: [(Int, Int)] -> Maybe [(Int, Int)]
+chain []            = Just []
+chain ((l, r) : xs) = ((l, r) :) <$> chain' l r xs
+
+-- | Compute a domino chain whose first number is `l`.
 chain' :: Int -> Int -> [(Int, Int)] -> Maybe [(Int, Int)]
+-- Check that we match on the wrap-around.
 chain' l r [] = if l == r then Just [] else Nothing
-chain' l r xs = msum $ map try xs
+chain' l r xs = MB.listToMaybe $ MB.mapMaybe go xs
   where
-    -- Try using `x` as the next domino (in both orientations).
-    try :: (Int, Int) -> Maybe [(Int, Int)]
-    try x@(l', r')
-      | l' == r   = (x :) <$> chain' l r' (delete x xs)
-      | r' == r   = (swap x :) <$> chain' l l' (delete x xs)
+    go :: (Int, Int) -> Maybe [(Int, Int)]
+    go x@(l', r')
+      -- Consume `l'` (which means we must match on `r'` next).
+      | r == l' = (x :) <$> chain' l r' (delete x xs)
+      -- Consume `r'` (which means we must match on `l'` next).
+      | r == r' = (swap x :) <$> chain' l l' (delete x xs)
       | otherwise = Nothing
