8405d65ef70f3ef9e61a0e28bdf18107a658be89
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-16 20:31:32 -0400

    Add someone else's solution to RE

--- alphametics/src/Alphametics.hs
+++ alphametics/src/Alphametics.hs
@@ -1,54 +1,31 @@
 module Alphametics (solve) where
 
-import Data.Map (Map)
+import Data.List ((\\))
+
+import qualified Data.Char as C
 import qualified Data.List as L
-import qualified Data.Map as M
-
-
-import Debug.Trace (trace)
-
-data Node = Node (Char, Int) [Node]
-
-ttt :: Show a => String -> a -> a
-ttt s x = trace (s ++ ": " ++ show x) x
-
-splitAtEq :: String -> (String, String)
-splitAtEq s = (expression, result)
-  where
-    s' = reverse s
-    result = (reverse . takeWhile (/= ' ')) s'
-    expression = (reverse . dropWhile (`elem` " =") . dropWhile (/= ' ')) s'
-
-splitIntoSummands :: String -> [String]
-splitIntoSummands "" = []
-splitIntoSummands s = summand : splitIntoSummands rest
-  where
-    summand = takeWhile (/= ' ') s
-    rest = (dropWhile (`elem` " +") . dropWhile (/= ' ')) s
+import qualified Data.Maybe as MB
 
 solve :: String -> Maybe [(Char, Int)]
-solve puzzle = dfs $ buildGraph (M.empty, Node ' ' 0, Node ' ' 0) equations
+solve puzzle = L.find (`solves` terms) maps
   where
-    (expression, result) = splitAtEq puzzle
-    summands = splitIntoSummands expression
-    columns = (L.transpose . map reverse) summands
-    result' = reverse result
-    equations = zip columns result'
+    maps     = map (zip ls') $ filter (zerosAfter l) $ L.permutations [0 .. 9]
+    l        = length initials
+    ls'      = initials ++ (letters \\ initials)
+    initials = L.nub $ map head terms
+    letters  = L.nub $ concat terms
+    terms    = takeWhile (not . null) $ L.unfoldr f puzzle
+      where
+        f cs = return (term, rest')
+          where
+            (term, rest ) = span C.isUpper cs
+            (_   , rest') = break C.isUpper rest
 
-dfs :: Node -> Maybe [(Char, Int)]
-dfs (Node (c, i) []) = Just [(c, i)]
-dfs (Node (c, i) (x : xs)) = Nothing
+zerosAfter :: Int -> [Int] -> Bool
+zerosAfter l p = 0 `notElem` take l p
 
-buildGraph :: Map Char Int -> [([Char], Char)] -> 
-buildGraph ((cs, d) : es) = Node ('a', 0) []
-
--- Instead, can we represent the problem as a graph? Each node is a (letter,
--- digit) pair, an assignment of a digit value to some letter. We start at a
--- null node, and then walk from letter to letter in the order of the
--- grade-school addition algorithm. We skip letters that we've already
--- assigned, and we skip letters whose values are determined by the addition
--- algorithm (below the 'sum' line). When we reach a leaf, this either
--- represents an unsatisfiable set of constraints (where we include our digit
--- assigments as 'constraints'), or a finished problem. When we reach a letter
--- for which there are multiple possible assignments, we do DFS, treating each
--- possible assignment as a child of the previous node.
+solves :: [(Char, Int)] -> [String] -> Bool
+solves cmap terms = sum (toInt <$> init terms) == toInt (last terms)
+  where
+    toInt xs = fromDigits $ MB.fromJust . (`lookup` cmap) <$> xs
+    fromDigits = L.foldl' (\n x -> n * 10 + x) 0
