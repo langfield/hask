245f1611899430e772064c147f5499419fa83d85
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-26 20:25:28 -0400

    Add some tracing code to understand what's going on

--- alphametics/src/Alphametics.hs
+++ alphametics/src/Alphametics.hs
@@ -6,15 +6,23 @@ import qualified Data.Char as C
 import qualified Data.List as L
 import qualified Data.Maybe as MB
 
+import Debug.Trace (trace)
+
+trace' :: Show a => String -> a -> a
+trace' s x = trace (s ++ ": " ++ show x) x
+
+-- | Find every map which solves `terms`.
+--
+-- A map is an assignment of digits to characters.
 solve :: String -> Maybe [(Char, Int)]
 solve puzzle = L.find (`solves` terms) maps
   where
     maps     = map (zip ls') $ filter (zerosAfter l) $ L.permutations [0 .. 9]
     l        = length initials
     ls'      = initials ++ (letters \\ initials)
-    initials = L.nub $ map head terms
+    initials = trace' "initials" $ L.nub $ map head terms
     letters  = L.nub $ concat terms
-    terms    = takeWhile (not . null) $ L.unfoldr f puzzle
+    terms    = trace' "terms" $ takeWhile (not . null) $ L.unfoldr f puzzle
       where
         f cs = return (term, rest')
           where
@@ -22,7 +30,7 @@ solve puzzle = L.find (`solves` terms) maps
             (_   , rest') = break C.isUpper rest
 
 zerosAfter :: Int -> [Int] -> Bool
-zerosAfter l p = 0 `notElem` take l p
+zerosAfter l p = 0 `notElem` trace' "permutation prefix" (take l (trace' "permutation" p))
 
 solves :: [(Char, Int)] -> [String] -> Bool
 solves cmap terms = sum (toInt <$> init terms) == toInt (last terms)
