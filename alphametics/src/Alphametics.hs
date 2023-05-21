5af529e44b7728b75d916a56b30b2d8ae51f80f0
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-05-20 20:11:53 -0400

    Add names to things

--- alphametics/src/Alphametics.hs
+++ alphametics/src/Alphametics.hs
@@ -8,27 +8,27 @@ import qualified Data.List as L
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Maybe as MB
 
--- | Find every map which solves `terms`.
---
--- A map is an assignment of digits to characters.
 solve' :: NonEmpty String -> Maybe [(Char, Int)]
-solve' terms =
-  L.find (`solves` terms)
-    . map (zip $ initials ++ (L.nub (concat terms) \\ initials))
-    . filter (\p -> 0 `notElem` take (length initials) p)
-    . L.permutations
-    $ [0 .. 9]
+solve' terms = L.find (`solves` terms) . map (zip letters) . filter (noZerosPriorTo n) . L.permutations $ [0 .. 9]
   where
-    initials = (L.nub . MB.catMaybes . NE.toList . NE.map MB.listToMaybe) terms
+    initials    = L.nub . MB.catMaybes . NE.toList . NE.map MB.listToMaybe $ terms
+    nonInitials = L.nub (concat terms) \\ initials
+    letters     = initials ++ nonInitials
+    n           = length initials
+
+noZerosPriorTo :: Int -> [Int] -> Bool
+noZerosPriorTo n p = 0 `notElem` take n p
 
 solve :: String -> Maybe [(Char, Int)]
-solve puzzle = solve' =<< NE.nonEmpty (filter (all C.isUpper) $ words puzzle)
+solve puzzle = solve' =<< NE.nonEmpty terms
+  where
+    terms = filter (all C.isUpper) . words $ puzzle
 
 solves :: [(Char, Int)] -> NonEmpty String -> Bool
-solves cmap terms = (sum <$> mapM toInt (NE.init terms)) == toInt (NE.last terms)
+solves cmap terms = (sum <$> mapM termToInt (NE.init terms)) == termToInt (NE.last terms)
   where
-    toInt :: [Char] -> Maybe Int
-    toInt xs = fromDigits <$> mapM (`lookup` cmap) xs
+    termToInt :: [Char] -> Maybe Int
+    termToInt xs = fromDigits <$> mapM (`lookup` cmap) xs
 
     fromDigits :: [Int] -> Int
     fromDigits = L.foldl' (\n x -> n * 10 + x) 0
