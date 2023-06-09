1cbf1c61eb0e1b8144d554ffbfd65a99a5747ccd
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-06-09 09:42:30 -0400

    Autoformat `Alphametics.hs`

--- alphametics/src/Alphametics.hs
+++ alphametics/src/Alphametics.hs
@@ -9,20 +9,19 @@ import qualified Data.List.NonEmpty as NE
 import qualified Data.Maybe as MB
 
 solve' :: NonEmpty String -> Maybe [(Char, Int)]
-solve' terms = L.find (`solves` terms) . map (zip letters) . filter (noZerosPriorTo n) . L.permutations $ [0 .. 9]
+solve' terms = findFirstSolution . zipLettersAndDigits . filterLeadingZeroes . L.permutations $ [0 .. 9]
   where
-    initials    = L.nub . MB.catMaybes . NE.toList . NE.map MB.listToMaybe $ terms
-    nonInitials = L.nub (concat terms) \\ initials
-    letters     = initials ++ nonInitials
-    n           = length initials
-
-noZerosPriorTo :: Int -> [Int] -> Bool
-noZerosPriorTo n p = 0 `notElem` take n p
+    initials            = L.nub . MB.catMaybes . NE.toList . NE.map MB.listToMaybe $ terms
+    nonInitials         = L.nub (concat terms) \\ initials
+    letters             = initials ++ nonInitials
+    n                   = length initials
+    findFirstSolution   = L.find (`solves` terms)
+    zipLettersAndDigits = map (zip letters)
+    filterLeadingZeroes = filter noZerosPriorTo
+    noZerosPriorTo p = 0 `notElem` take n p
 
 solve :: String -> Maybe [(Char, Int)]
-solve puzzle = solve' =<< NE.nonEmpty terms
-  where
-    terms = filter (all C.isUpper) . words $ puzzle
+solve puzzle = solve' =<< NE.nonEmpty terms where terms = filter (all C.isUpper) . words $ puzzle
 
 solves :: [(Char, Int)] -> NonEmpty String -> Bool
 solves cmap terms = (sum <$> mapM termToInt (NE.init terms)) == termToInt (NE.last terms)
