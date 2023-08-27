412d179d32650b93c8761938454b3a80d836fdb4
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-08-27 12:30:13 -0400

    Cleanup

--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -3,8 +3,11 @@ module Connect (Mark(..), winner, hexagons) where
 import qualified Data.List as L
 import Data.Map (Map)
 import qualified Data.Map as M
+import qualified Data.Maybe as MB
 import Data.Set (Set)
 import qualified Data.Set as S
+
+-- We map each element in the 2D array to a list of its 6 neighbors.
 
 data Mark = Cross | Nought deriving (Eq, Show)
 type Board = [String]
@@ -18,56 +21,12 @@ instance Show Hex where
   show (Empty x y) = "Empty(" ++ show x ++ ", " ++ show y ++ ")"
   show (XO p x y ) = show p ++ "(" ++ show x ++ ", " ++ show y ++ ")"
 
-winner :: [String] -> Maybe Mark
-winner board
-  | won X (L.transpose board') = Just Cross
-  | won O board' = Just Nought
-  | otherwise = Nothing
-  where
-    board' = map (filter (/= ' ')) board
-
--- | Check if player `c` has connected top-to-bottom.
---
--- The easiest way to do this is just to return `True` when we've reached the
--- last row. This should probably be a simple DFS function, right?
-won :: Player -> Board -> Bool
-won c board = or outcomes
-  where
-    hexs     = mkhexs board
-    graph    = hexagons Null hexs
-    starts   = filter (isPlayer c) . head $ hexs
-    targets  = S.fromList . filter (isPlayer c) . last $ hexs
-    outcomes = [ search c graph targets start S.empty | start <- starts ]
-
 isPlayer :: Player -> Hex -> Bool
 isPlayer _  Null        = False
 isPlayer _  (Empty _ _) = False
 isPlayer p' (XO p _ _ ) = p == p'
 
-search :: Player -> Map Hex [Hex] -> Set Hex -> Hex -> Set Hex -> Bool
-search _ _ _ (Empty _ _) _ = False
-search _ _ _ Null        _ = False
-search p' graph targets start@(XO p _ _) visiting
-  | p /= p' = False
-  | start `S.member` targets = seq start True
-  | otherwise = case nbs of
-    Just nbs' ->
-      or [ search p' graph targets nb (S.insert nb visiting) | nb <- nbs' ]
-    Nothing -> False
-  where
-    nbs =
-      filter (isPlayer p')
-        .   filter (`S.notMember` visiting)
-        <$> M.lookup start graph
-
--- We essentially want to iterate over something that gives us a 7-tuple, where
--- we get the current element, and its 6 neighbors. And then we can put them
--- all in a data structure.
---
--- One good way to start might be to solve the 1-dimensional case first, where
--- we have a list and we want to get 3-tuples of each element and its
--- neighbors.
-
+-- | Right neighbors in a list.
 rights :: Ord a => a -> [a] -> Map a [a]
 rights _ []  = M.empty
 rights _ [x] = M.singleton x []
@@ -75,12 +34,15 @@ rights c (x : y : rest)
   | y /= c    = M.insert x [y] (rights c (y : rest))
   | otherwise = M.insert x [] (rights c (y : rest))
 
+-- | Left neighbors in a list.
 lefts :: Ord a => a -> [a] -> Map a [a]
 lefts c = rights c . reverse
 
+-- | Left and right neighbors in a list.
 neighbors :: Ord a => a -> [a] -> Map a [a]
 neighbors c xs = M.unionWith (++) (lefts c xs) (rights c xs)
 
+-- | 4-way neighbors in a 2D grid.
 neighbors2D :: Ord a => a -> [[a]] -> Map a [a]
 neighbors2D c xss = M.unionWith (++) horizontals verticals
   where
@@ -98,6 +60,7 @@ stagger' n c (xs : xss)
 stagger :: Ord a => a -> [[a]] -> [[a]]
 stagger = stagger' 0
 
+-- | Top-right and bottom-left neighbors in a 2D grid.
 diagonals :: Ord a => a -> [[a]] -> Map a [a]
 diagonals c =
   foldr (M.unionWith (++) . neighbors c) M.empty . L.transpose . stagger c
@@ -110,17 +73,53 @@ map' = go 0
     go _ _ []       = []
     go i f (x : xs) = f i x : go (i + 1) f xs
 
+-- | Construct a Hex from coordinates.
 mkhex :: Int -> Int -> Char -> Hex
 mkhex i j 'X' = XO X i j
 mkhex i j 'O' = XO O i j
 mkhex i j _   = Empty i j
 
+-- | Construct 2D grid of Hexs from a board.
 mkhexs :: [[Char]] -> [[Hex]]
 mkhexs = map' go
   where
     go :: Int -> [Char] -> [Hex]
     go i = map' (mkhex i)
 
+-- | Construct graph of hexagonal neighbors from a 2D grid of Hexs.
+--
+-- Note that `c` is used to left-pad rows in `stagger`.
 hexagons :: Hex -> [[Hex]] -> Map Hex [Hex]
 hexagons c xss =
   M.delete c . M.unionWith (++) (neighbors2D c xss) . diagonals c $ xss
+
+-- | Check if a target is reachable from a start Hex as a given player.
+search :: Player -> Map Hex [Hex] -> Set Hex -> Hex -> Set Hex -> Bool
+search _ _ _ Null        _ = False
+search _ _ _ (Empty _ _) _ = False
+search p' graph targets start@(XO p _ _) visiting
+  | p /= p' = False
+  | start `S.member` targets = True
+  | otherwise = or
+    [ search p' graph targets nb (S.insert nb visiting) | nb <- nbs' ]
+  where
+    nbs  = MB.fromMaybe [] (M.lookup start graph)
+    nbs' = filter (isPlayer p') . filter (`S.notMember` visiting) $ nbs
+
+-- | Check if player `c` has connected top-to-bottom.
+won :: Player -> Board -> Bool
+won c board = or outcomes
+  where
+    hexs     = mkhexs board
+    graph    = hexagons Null hexs
+    starts   = filter (isPlayer c) . head $ hexs
+    targets  = S.fromList . filter (isPlayer c) . last $ hexs
+    outcomes = [ search c graph targets start S.empty | start <- starts ]
+
+-- | Compute the Mark of the winner of a given board.
+winner :: [String] -> Maybe Mark
+winner board
+  | won X (L.transpose board') = Just Cross
+  | won O board'               = Just Nought
+  | otherwise                  = Nothing
+  where board' = map (filter (/= ' ')) board
