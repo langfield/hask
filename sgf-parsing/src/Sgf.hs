10893bf5fb212852e4e65fcf3991226d59bed342
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-09-10 21:58:56 -0400

    Add a megaparsec skeleton

--- sgf-parsing/src/Sgf.hs
+++ sgf-parsing/src/Sgf.hs
@@ -1,8 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Sgf (parseSgf) where
 
 import Data.Map  (Map)
+import Data.Void (Void)
 import Data.Text (Text)
-import Data.Tree (Tree)
+import Data.Tree (Tree (..))
+import Text.Megaparsec (Parsec)
+
+import qualified Data.Text as T
+import qualified Data.Map as M
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MPC
+
+type Parser = Parsec Void Text
+
+-- The semicolon kinda means, "Everything after this is a single child node of
+-- the previous node."
 
 parseSgf :: Text -> Maybe (Tree (Map Text [Text]))
-parseSgf sgf = Nothing
+parseSgf = MP.parseMaybe parseNothing
+
+parseNothing :: Parser (Tree (Map Text [Text]))
+parseNothing = do
+  _ <- MPC.char '('
+  _ <- MPC.char ';'
+  _ <- MPC.char ')'
+  pure (Node M.empty [])
