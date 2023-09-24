4141e1eb993adc231b49220b56b027322cadfcea
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-09-23 21:36:16 -0400

    qnikst's solution

--- zipper/src/Zipper.hs
+++ zipper/src/Zipper.hs
@@ -1,46 +1,72 @@
-module Zipper
- ( BinTree(BT)
- , fromTree
- , left
- , right
- , setLeft
- , setRight
- , setValue
- , toTree
- , up
- , value
- ) where
+module Zipper (
+    BinTree(..),
+    Zipper,
+    fromTree,
+    toTree,
+    value,
+    left,
+    right,
+    up,
+    setValue,
+    setLeft,
+    setRight
+) where
+import Data.List (foldl')
 
-data BinTree a = BT { btValue :: a
-                    , btLeft  :: Maybe (BinTree a)
-                    , btRight :: Maybe (BinTree a)
-                    } deriving (Eq, Show)
+-- | A binary tree.
+data BinTree a = BT { 
+    btValue :: a                 -- ^ Value
+  , btLeft  :: Maybe (BinTree a) -- ^ Left child
+  , btRight :: Maybe (BinTree a) -- ^ Right child
+} deriving (Eq, Show)
 
-data Zipper a = Dummy deriving (Eq, Show)
+type T a = (a, Maybe (BinTree a))
 
+-- | A zipper for a binary tree.
+data Zipper a = Zipper 
+      { zValue :: a
+      , zLeft  :: Maybe (BinTree a)
+      , zRight :: Maybe (BinTree a)
+      , zTop   :: [Either (T a) (T a)]
+      } deriving (Show, Eq)
+
+-- | Get a zipper focussed on the root node.
 fromTree :: BinTree a -> Zipper a
-fromTree tree = error "You need to implement this function."
+fromTree (BT x l r) = Zipper x l r []
 
+-- | Get the complete tree from a zipper.
 toTree :: Zipper a -> BinTree a
-toTree zipper = error "You need to implement this function."
+toTree (Zipper x l r t) = foldl' go (BT x l r) t
+  where
+    go b (Left (x',l')) = BT x' l' (Just b)
+    go b (Right (x',r')) = BT x' (Just b) r'
 
+-- | Get the value of the focus node.
 value :: Zipper a -> a
-value zipper = error "You need to implement this function."
+value = zValue
 
+-- | Get the left child of the focus node, if any.
 left :: Zipper a -> Maybe (Zipper a)
-left zipper = error "You need to implement this function."
+left (Zipper a l r t) = fmap (\(BT x  l' r') -> Zipper x l' r' (Right (a,r):t)) l
 
+-- | Get the right child of the focus node, if any.
 right :: Zipper a -> Maybe (Zipper a)
-right zipper = error "You need to implement this function."
+right (Zipper a l r t) = fmap (\(BT x l' r') -> Zipper x l' r' (Left (a,l):t)) r
 
+-- | Get the parent of the focus node, if any.
 up :: Zipper a -> Maybe (Zipper a)
-up zipper = error "You need to implement this function."
+up (Zipper _ _ _ []) = Nothing
+up (Zipper a l r (Left (x,l'):ts))  = Just $ Zipper x l' (Just $ BT a l r) ts
+up (Zipper a l r (Right (x,r'):ts)) = Just $ Zipper x (Just $ BT a l r) r' ts
 
+-- | Set the value of the focus node.
 setValue :: a -> Zipper a -> Zipper a
-setValue x zipper = error "You need to implement this function."
+setValue x z = z{zValue = x}
 
+-- | Replace a left child tree.
 setLeft :: Maybe (BinTree a) -> Zipper a -> Zipper a
-setLeft tree zipper = error "You need to implement this function."
+setLeft l z = z{zLeft = l} 
 
+-- | Replace a right child tree.
 setRight :: Maybe (BinTree a) -> Zipper a -> Zipper a
-setRight tree zipper = error "You need to implement this function."
+setRight r z = z{zRight = r}
