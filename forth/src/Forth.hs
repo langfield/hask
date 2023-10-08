7c3213fe2f5361deede293ac866f810ce07edf5b
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-10-08 17:50:29 -0400

    First working Forth

--- forth/src/Forth.hs
+++ forth/src/Forth.hs
@@ -8,7 +8,7 @@ module Forth
   , emptyState
   ) where
 
-import Control.Monad (foldM)
+import Control.Monad ((>=>))
 import Data.Map (Map)
 import Data.Text (Text)
 
@@ -28,51 +28,80 @@ data ForthError
      | UnknownWord Text
      deriving (Show, Eq)
 
-emptyState :: ForthState
-emptyState = ForthState [] M.empty
-
-data Token = Colon | Semi | Add | Sub | Mul | Div | Dup | Drop | Swap | Over | Word String | Number Int
+data Token = Colon | Semi | Word String
   deriving (Eq, Show)
 
 -- Function calls just push their implementation onto the stack.
-type Namespace = Map String [Token]
+type Namespace = Map String ([Int] -> Either ForthError [Int])
 data ForthState = ForthState [Int] Namespace
 
 parse :: String -> Token
 parse ":" = Colon
 parse ";" = Semi
-parse "+" = Add
-parse "-" = Sub
-parse "*" = Mul
-parse "/" = Div
-parse "dup" = Dup
-parse "drop" = Drop
-parse "swap" = Swap
-parse "over" = Over
-parse x
-  | all C.isDigit x = Number (read x)
-  | otherwise = Word x
+parse x = Word x
+
+add' :: [Int] -> Either ForthError [Int]
+add' (x : y : rest) = Right (x + y : rest)
+add' _ = Left StackUnderflow
+
+sub' :: [Int] -> Either ForthError [Int]
+sub' (x : y : rest) = Right (y - x : rest)
+sub' _ = Left StackUnderflow
+
+mul' :: [Int] -> Either ForthError [Int]
+mul' (x : y : rest) = Right (x * y : rest)
+mul' _ = Left StackUnderflow
+
+div' :: [Int] -> Either ForthError [Int]
+div' (0 : _ : _) = Left DivisionByZero
+div' (x : y : rest) = Right (y `div` x : rest)
+div' _ = Left StackUnderflow
+
+dup' :: [Int] -> Either ForthError [Int]
+dup' (x : rest) = Right (x : x : rest)
+dup' _ = Left StackUnderflow
+
+drop' :: [Int] -> Either ForthError [Int]
+drop' (_ : rest) = Right rest
+drop' _ = Left StackUnderflow
+
+swap' :: [Int] -> Either ForthError [Int]
+swap' (x : y : rest) = Right (y : x : rest)
+swap' _ = Left StackUnderflow
+
+over' :: [Int] -> Either ForthError [Int]
+over' (x : y : rest) = Right (y : x : y : rest)
+over' _ = Left StackUnderflow
+
+emptyState :: ForthState
+emptyState = ForthState [] $ M.fromList [("+", add'), ("-", sub'), ("*", mul'), ("/", div'), ("dup", dup'), ("drop", drop'), ("swap", swap'), ("over", over')]
+
+getDef :: Namespace -> Token -> Either ForthError ([Int] -> Either ForthError [Int])
+getDef defs (Word name)
+  | all C.isDigit name = Right (\xs -> Right (read name : xs))
+  | otherwise =
+    case M.lookup (map C.toLower name) defs of
+      Just def -> Right def
+      Nothing -> Left $ UnknownWord (T.pack name)
+getDef _ _ = Left InvalidWord
 
 go :: ForthState -> [Token] -> Either ForthError ForthState
 go stack [] = Right stack
-go (ForthState xs defs) ((Number x) : ts) = go (ForthState (x : xs) defs) ts
-go (ForthState (x : y : rest) defs) (Add : ts) = go (ForthState (x + y : rest) defs) ts
-go (ForthState (x : y : rest) defs) (Sub : ts) = go (ForthState (y - x : rest) defs) ts
-go (ForthState (x : y : rest) defs) (Mul : ts) = go (ForthState (x * y : rest) defs) ts
-go (ForthState (0 : _ : _) _) (Div : _) = Left DivisionByZero
-go (ForthState (x : y : rest) defs) (Div : ts) = go (ForthState (y `div` x : rest) defs) ts
-go (ForthState (x : rest) defs) (Dup : ts) = go (ForthState (x : x : rest) defs) ts
-go (ForthState (_ : rest) defs) (Drop : ts) = go (ForthState rest defs) ts
-go (ForthState (x : y : rest) defs) (Swap : ts) = go (ForthState (y : x : rest) defs) ts
-go (ForthState (x : y : rest) defs) (Over : ts) = go (ForthState (y : x : y : rest) defs) ts
-go (ForthState xs defs) (Colon : Word w : ts) = go (ForthState xs defs') ts'
-  where
-    (def, ts') = break (== Semi) ts
-    defs' = M.insert w def defs
+go (ForthState xs defs) (Colon : Word w : ts)
+  | all C.isDigit w = Left InvalidWord
+  | otherwise =
+    case foldr (>=>) Right <$> mapM (getDef defs) ws of
+      Right def -> go (ForthState xs (M.insert (map C.toLower w) def defs)) ts'
+      Left err -> Left err
+    where
+      (ws, ts') = break (== Semi) ts
 go (ForthState xs defs) (Word w : ts) =
-  case M.lookup w defs of
-    Just def -> go (ForthState xs defs) (def ++ ts)
-    Nothing -> Left $ UnknownWord (T.pack w)
+  case getDef defs (Word w) of
+    Right def ->
+      case def xs of
+        Right xs' -> go (ForthState xs' defs) ts
+        Left err -> Left err
+    Left err -> Left err
 go stack (Semi : ts) = go stack ts
 go (ForthState _ _) _ = Left StackUnderflow
 
