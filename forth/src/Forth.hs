1331122fad4f3055fd44c267004abaab6840b0ff
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-10-09 21:25:14 -0400

    Reformat

--- forth/src/Forth.hs
+++ forth/src/Forth.hs
@@ -1,19 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Forth
-  ( ForthError(..)
-  , ForthState
-  , evalText
-  , toList
-  , emptyState
-  ) where
+module Forth (ForthError(..), ForthState, evalText, toList, emptyState) where
 
-import Control.Monad ((>=>), (<=<))
+import Control.Monad ((<=<), (>=>))
 import Data.Map (Map)
 import Data.Text (Text)
 
-import qualified Data.Map as M
 import qualified Data.Char as C
+import qualified Data.Map as M
 import qualified Data.Text as T
 
 data ForthError
@@ -33,7 +27,7 @@ data ForthState = ForthState [Int] Namespace
 parse :: String -> Token
 parse ":" = Colon
 parse ";" = Semi
-parse x = Word x
+parse x   = Word x
 
 add' :: [Int] -> Either ForthError [Int]
 add' (x : y : rest) = Right (x + y : rest)
@@ -69,14 +63,22 @@ over' (x : y : rest) = Right (y : x : y : rest)
 over' _ = Left StackUnderflow
 
 emptyState :: ForthState
-emptyState = ForthState [] $ M.fromList [("+", add'), ("-", sub'), ("*", mul'), ("/", div'), ("dup", dup'), ("drop", drop'), ("swap", swap'), ("over", over')]
+emptyState = ForthState [] $ M.fromList
+  [ ("+"   , add')
+  , ("-"   , sub')
+  , ("*"   , mul')
+  , ("/"   , div')
+  , ("dup" , dup')
+  , ("drop", drop')
+  , ("swap", swap')
+  , ("over", over')
+  ]
 
 getDef :: Namespace -> String -> Either ForthError ([Int] -> Either ForthError [Int])
 getDef defs w
   | all C.isDigit w = Right (\xs -> Right (read w : xs))
   | otherwise = maybe unknown Right (M.lookup w defs)
-  where
-    unknown = Left . UnknownWord . T.pack $ w
+  where unknown = Left . UnknownWord . T.pack $ w
 
 wordName :: Token -> Either ForthError String
 wordName (Word name) = Right name
@@ -86,22 +88,22 @@ go :: [Int] -> Namespace -> [Token] -> Either ForthError ForthState
 go xs defs [] = Right (ForthState xs defs)
 go xs defs (Semi : ts) = go xs defs ts
 go xs defs (Word w : ts) = getDef defs w >>= run
-  where
-    run def = def xs >>= (\xs' -> go xs' defs ts)
+  where run def = def xs >>= (\xs' -> go xs' defs ts)
 go xs defs (Colon : Word w : ts)
   | all C.isDigit w = Left InvalidWord
   | otherwise = define . composeMany =<< mapM (getDef defs <=< wordName) ws
-    where
-      (ws, ts') = break (== Semi) ts
-      composeMany = foldr (>=>) Right
-      define def = go xs (M.insert w def defs) ts'
+  where
+    (ws, ts')   = break (== Semi) ts
+    composeMany = foldr (>=>) Right
+    define def = go xs (M.insert w def defs) ts'
 go _ _ _ = Left StackUnderflow
 
 rev :: ForthState -> ForthState
 rev (ForthState xs m) = ForthState (reverse xs) m
 
 evalText :: Text -> ForthState -> Either ForthError ForthState
-evalText text (ForthState xs defs) = fmap rev . go xs defs . map parse . words . map C.toLower . T.unpack $ text
+evalText text (ForthState xs defs) =
+  fmap rev . go xs defs . map parse . words . map C.toLower . T.unpack $ text
 
 toList :: ForthState -> [Int]
 toList (ForthState stack _) = stack
