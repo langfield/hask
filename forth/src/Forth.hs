a1eeadfcaace7ff9756296e5d0cdc7503fe79a82
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-10-10 21:35:18 -0400

    Clean up things a bit

--- forth/src/Forth.hs
+++ forth/src/Forth.hs
@@ -3,7 +3,6 @@ module Forth (ForthError (..), ForthState, evalText, toList, emptyState) where
 import Data.Char (isNumber, toLower)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromJust, isJust)
 import Data.Text (Text)
 import qualified Data.Text as T
 
@@ -25,12 +24,13 @@ parseToken token
   | T.all isNumber token = Number . read . T.unpack $ token
   | otherwise = Symbol . T.map toLower $ token
 
-resolve :: Map Text [Token] -> [Token] -> [Token]
-resolve _ [] = []
-resolve vars (Number n : expr) = Number n : resolve vars expr
-resolve vars (Symbol s : expr) = case M.lookup s vars of
-  Just def -> def ++ resolve vars expr
-  Nothing -> Symbol s : resolve vars expr
+-- | Inline word definitions (but only 1 level deep).
+inline :: Map Text [Token] -> [Token] -> [Token]
+inline _ [] = []
+inline vars (Number n : expr) = Number n : inline vars expr
+inline vars (Symbol s : expr) = case M.lookup s vars of
+  Just def -> def ++ inline vars expr
+  Nothing -> Symbol s : inline vars expr
 
 isBuiltin :: Text -> Bool
 isBuiltin sym = sym `elem` ["dup", "drop", "swap", "over", "+", "-", "/", "*"]
@@ -38,27 +38,25 @@ isBuiltin sym = sym `elem` ["dup", "drop", "swap", "over", "+", "-", "/", "*"]
 -- | Recursively evaluate a token stream given some state.
 eval :: [Token] -> ForthState -> Either ForthError ForthState
 eval [] result = Right result
-eval (Number n : os) (vars, xs) = eval os (vars, n : xs)
-eval (Symbol s : os) (vars, xs)
-  | isJust def = eval (fromJust def ++ os) (vars, xs)
-  where
-    def = M.lookup s vars
+eval (Number n : ts) (vars, xs) = eval ts (vars, n : xs)
+eval (Symbol s : ts) (vars, xs)
+  | Just def <- M.lookup s vars = eval (def ++ ts) (vars, xs)
 eval (Symbol ":" : Number _ : _) (_, _) = Left InvalidWord
-eval (Symbol ":" : os) (vars, xs) = eval rest (newVars, xs)
+eval (Symbol ":" : ts) (vars, xs) = eval rest (vars', xs)
   where
-    newVars = M.insert s (resolve vars definition) vars
-    (Symbol s : definition, Symbol ";" : rest) = span (/= Symbol ";") os
-eval (Symbol "dup" : os) (vars, x : xs) = eval os (vars, x : x : xs)
-eval (Symbol "drop" : os) (vars, _ : xs) = eval os (vars, xs)
-eval (Symbol "swap" : os) (vars, x : y : rest) = eval os (vars, y : x : rest)
-eval (Symbol "over" : os) (vars, x : y : rest) = eval os (vars, y : x : y : rest)
-eval (Symbol "+" : os) (vars, x : y : rest) = eval os (vars, (y + x) : rest)
-eval (Symbol "-" : os) (vars, x : y : rest) = eval os (vars, (y - x) : rest)
-eval (Symbol "*" : os) (vars, x : y : rest) = eval os (vars, (y * x) : rest)
+    vars' = M.insert s (inline vars def) vars
+    (Symbol s : def, Symbol ";" : rest) = span (/= Symbol ";") ts
+eval (Symbol "dup" : ts) (vars, x : xs) = eval ts (vars, x : x : xs)
+eval (Symbol "drop" : ts) (vars, _ : xs) = eval ts (vars, xs)
+eval (Symbol "swap" : ts) (vars, x : y : rest) = eval ts (vars, y : x : rest)
+eval (Symbol "over" : ts) (vars, x : y : rest) = eval ts (vars, y : x : y : rest)
+eval (Symbol "+" : ts) (vars, x : y : rest) = eval ts (vars, (y + x) : rest)
+eval (Symbol "-" : ts) (vars, x : y : rest) = eval ts (vars, (y - x) : rest)
+eval (Symbol "*" : ts) (vars, x : y : rest) = eval ts (vars, (y * x) : rest)
 eval (Symbol "/" : _) (_, 0 : _ : _) = Left DivisionByZero
-eval (Symbol "/" : os) (vars, x : y : rest) = eval os (vars, (y `div` x) : rest)
-eval (Symbol s : _) (vars, _)
-  | M.notMember s vars && not (isBuiltin s) = Left $ UnknownWord s
+eval (Symbol "/" : ts) (vars, x : y : rest) = eval ts (vars, (y `div` x) : rest)
+eval (Symbol s : _) _
+  | not (isBuiltin s) = Left (UnknownWord s)
   | otherwise = Left StackUnderflow
 
 evalText :: Text -> ForthState -> Either ForthError ForthState
