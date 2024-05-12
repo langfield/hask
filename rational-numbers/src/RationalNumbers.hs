a5da507d0af1177dbdac01a7fe227bace5625a6a
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-12 12:48:59 -0400

    Complete `rational-numbers`

--- rational-numbers/src/RationalNumbers.hs
+++ rational-numbers/src/RationalNumbers.hs
@@ -19,36 +19,43 @@ import qualified Prelude
 newtype Rational a = Rational (a, a) deriving (Eq, Show)
 
 rational :: Integral a => (a, a) -> Rational a
-rational = Rational
+rational (x, y) = Rational (sgn * x' `Prelude.div` d, y' `Prelude.div` d)
+  where
+    d = gcd x y
+    x' = Prelude.abs x
+    y' = Prelude.abs y
+    sgn = if x*y < 0 then -1 else 1
 
 -- unary operators -------------------------------------------------------------
 abs :: Integral a => Rational a -> Rational a
 abs (Rational (a, b)) = Rational (Prelude.abs a, Prelude.abs b)
 
 numerator :: Integral a => Rational a -> a
-numerator = error "You need to implement this function"
+numerator (Rational (a, _)) = a
 
 denominator :: Integral a => Rational a -> a
-denominator = error "You need to implement this function"
+denominator (Rational (_, b)) = b
 
 -- binary operators ------------------------------------------------------------
 add :: Integral a => Rational a -> Rational a -> Rational a
-add = error "You need to implement this function"
+add (Rational (a, b)) (Rational (c, d)) = rational (a * d + c * b, b * d)
 
 sub :: Integral a => Rational a -> Rational a -> Rational a
-sub = error "You need to implement this function"
+sub (Rational (a, b)) (Rational (c, d)) = rational (a * d - c * b, b * d)
 
 mul :: Integral a => Rational a -> Rational a -> Rational a
-mul = error "You need to implement this function"
+mul (Rational (a, b)) (Rational (c, d)) = rational (a * c, b * d)
 
 div :: Integral a => Rational a -> Rational a -> Rational a
-div = error "You need to implement this function"
+div (Rational (a, b)) (Rational (c, d)) = rational (a * d, b * c)
 
-pow :: Integral a => Rational a -> a -> Rational a
-pow = error "You need to implement this function"
+pow :: (Show a, Integral a) => Rational a -> a -> Rational a
+pow (Rational (a, b)) n
+  | n >= 0 = rational (a ^ n, b ^ n)
+  | otherwise = rational (b ^ Prelude.abs n, a ^ Prelude.abs n)
 
-expRational :: Integral a => Floating b => Rational a -> b -> b
-expRational = error "You need to implement this function"
+expRational :: (Integral a, Floating b) => Rational a -> b -> b
+expRational (Rational (a, b)) n = (fromIntegral a ** n) / (fromIntegral b ** n)
 
 expReal :: Floating a => Integral b => a -> Rational b -> a
-expReal = error "You need to implement this function"
+expReal x (Rational (a, b)) = x ** (fromIntegral a / fromIntegral b)
