162911fa1a778358fa4d26e9b6bb32cb55481506
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-16 06:15:27 -0400

    Start rewriting `connect`

--- connect/README.md
+++ connect/README.md
@@ -7,19 +7,14 @@ If you need help running the tests or submitting your code, check out `HELP.md`.
 
 Compute the result for a game of Hex / Polygon.
 
-The abstract boardgame known as
-[Hex](https://en.wikipedia.org/wiki/Hex_%28board_game%29) / Polygon /
-CON-TAC-TIX is quite simple in rules, though complex in practice. Two players
-place stones on a parallelogram with hexagonal fields. The player to connect his/her
-stones to the opposite side first wins. The four sides of the parallelogram are
-divided between the two players (i.e. one player gets assigned a side and the
-side directly opposite it and the other player gets assigned the two other
-sides).
+The abstract boardgame known as [Hex][hex] / Polygon / CON-TAC-TIX is quite simple in rules, though complex in practice.
+Two players place stones on a parallelogram with hexagonal fields.
+The player to connect his/her stones to the opposite side first wins.
+The four sides of the parallelogram are divided between the two players (i.e. one player gets assigned a side and the side directly opposite it and the other player gets assigned the two other sides).
 
-Your goal is to build a program that given a simple representation of a board
-computes the winner (or lack thereof). Note that all games need not be "fair".
-(For example, players may have mismatched piece counts or the game's board might
-have a different width and height.)
+Your goal is to build a program that given a simple representation of a board computes the winner (or lack thereof).
+Note that all games need not be "fair".
+(For example, players may have mismatched piece counts or the game's board might have a different width and height.)
 
 The boards look like this:
 
@@ -31,50 +26,10 @@ The boards look like this:
     X O O O X
 ```
 
-```text
-. O . X .
-. X X O .
-O O O X .
-. X O X O
-X O O O X
-```
+"Player `O`" plays from top to bottom, "Player `X`" plays from left to right.
+In the above example `O` has made a connection from left to right but nobody has won since `O` didn't connect top and bottom.
 
-```text
-. O A . .
-O X O . .
-B O . . .
-. . . . .
-. . . . .
-```
-
-```text
-. O A . .
-- O X O . .
-- - B O . . .
-- - - . . . . .
-- - - - . . . . .
-```
-
-[ [1,  2, 3, 4]
-, [5,  6, 7, 8]
-, [9, 10,11,12]
-, [13,14,15,16]]
-
-
-Staggered and then transposed.
-
-[ [1,   ,  ,  ]
-, [2,  5,  ,  ]
-, [3,  6, 9,  ]
-, [4,  7,10,13]
-, [8, 11,14]
-, [12,15]
-, [16]
-]
-
-"Player `O`" plays from top to bottom, "Player `X`" plays from left to right. In
-the above example `O` has made a connection from left to right but nobody has
-won since `O` didn't connect top and bottom.
+[hex]: https://en.wikipedia.org/wiki/Hex_%28board_game%29
 
 ## Source
 
@@ -94,19 +49,3 @@ won since `O` didn't connect top and bottom.
 - @rbasso
 - @sshine
 - @tejasbubane
-
-## Notes
-
-Okay, so last time we implemented `won`. I believe this is pretty damn close to
-correct. So now we're focusing on `search`. Right now we basically have it
-sorried out. So we want to search from `(x, y)`, basically doing DFS looking
-for the bottom row.
-
-So we check if `i == m - 1`, which determines if we've reached the last row.
-And we also must check if `c == c'`, so we know we're on the right type of
-square. But there is a problem because without dependent types, we have no way
-of safely indexing into the list. So we need to do search in the nested list
-without indexing.
-
-It turns out, the way we probably want to do this is with a pair of nested
-zippers. So let's take a moment and review how zippers work in pure langauges.
--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -1,122 +1,21 @@
-module Connect (Mark(..), winner, hexagons) where
-
-import qualified Data.List as L
-import Data.Map (Map)
-import qualified Data.Map as M
-import qualified Data.Maybe as MB
-import Data.Set (Set)
-import qualified Data.Set as S
+module Connect (Mark(..), winner) where
 
 -- We map each element in the 2D array to a list of its 6 neighbors.
 
 data Mark = Cross | Nought deriving (Eq, Show)
-type Board = [String]
-data Player = X | O deriving (Eq, Ord, Show)
 
-data Hex = Null | Empty Int Int | XO Player Int Int
-  deriving (Eq, Ord)
+unstagger :: [[a]] -> [[a]]
+unstagger = zipWith drop [0..]
 
-instance Show Hex where
-  show Null        = "Null"
-  show (Empty x y) = "Empty(" ++ show x ++ ", " ++ show y ++ ")"
-  show (XO p x y ) = show p ++ "(" ++ show x ++ ", " ++ show y ++ ")"
-
-isPlayer :: Player -> Hex -> Bool
-isPlayer _  Null        = False
-isPlayer _  (Empty _ _) = False
-isPlayer p' (XO p _ _ ) = p == p'
-
--- | Right neighbors in a list.
-rights :: Ord a => a -> [a] -> Map a [a]
-rights _ []  = M.empty
-rights _ [x] = M.singleton x []
-rights c (x : y : rest)
-  | y /= c    = M.insert x [y] (rights c (y : rest))
-  | otherwise = M.insert x [] (rights c (y : rest))
-
--- | Left and right neighbors in a list.
-neighbors :: Ord a => a -> [a] -> Map a [a]
-neighbors c xs = M.unionWith (++) (lefts c xs) (rights c xs)
-  where lefts c' = rights c' . reverse
-
--- | 4-way neighbors in a 2D grid.
-neighbors2D :: Ord a => a -> [[a]] -> Map a [a]
-neighbors2D c xss = M.unionWith (++) leftrights updowns
+augment :: a -> [[a]] -> [[a]]
+augment d m = map (pad d) . pad (replicate ncols d) $ m
   where
-    leftrights = foldr (M.unionWith (++) . neighbors c) M.empty xss
-    updowns = foldr (M.unionWith (++) . neighbors c) M.empty (L.transpose xss)
+    pad d' = reverse . (d' :) . reverse . (d' :)
+    ncols = foldr (max . length) 0 m
 
-stagger' :: Ord a => Int -> a -> [[a]] -> [[a]]
-stagger' _ _ [] = []
-stagger' n c (xs : xss)
-  | n <= 0    = xs : stagger' 1 c xss
-  | otherwise = (prefix ++ xs) : stagger' (n + 1) c xss
-  where prefix = replicate n c
-
--- | Left-pad each row with `i` copies of the first argument.
-stagger :: Ord a => a -> [[a]] -> [[a]]
-stagger = stagger' 0
-
--- | Map with index of iteration.
-map' :: (Int -> a -> b) -> [a] -> [b]
-map' = go 0
-  where
-    go :: Int -> (Int -> a -> b) -> [a] -> [b]
-    go _ _ []       = []
-    go i f (x : xs) = f i x : go (i + 1) f xs
-
--- | Construct a Hex from coordinates.
-mkhex :: Int -> Int -> Char -> Hex
-mkhex i j 'X' = XO X i j
-mkhex i j 'O' = XO O i j
-mkhex i j _   = Empty i j
-
--- | Construct 2D grid of Hexs from a board.
-mkhexs :: [[Char]] -> [[Hex]]
-mkhexs = map' go
-  where
-    go :: Int -> [Char] -> [Hex]
-    go i = map' (mkhex i)
-
--- | Top-right and bottom-left neighbors in a 2D grid.
-diagonals :: Ord a => a -> [[a]] -> Map a [a]
-diagonals c =
-  foldr (M.unionWith (++) . neighbors c) M.empty . L.transpose . stagger c
-
--- | Construct graph of hexagonal neighbors from a 2D grid of Hexs.
---
--- Note that `c` is used to left-pad rows in `stagger`.
-hexagons :: Hex -> [[Hex]] -> Map Hex [Hex]
-hexagons c xss =
-  M.delete c . M.unionWith (++) (neighbors2D c xss) . diagonals c $ xss
-
--- | Check if a target is reachable from a start Hex as a given player.
-search :: Player -> Map Hex [Hex] -> Set Hex -> Hex -> Set Hex -> Bool
-search _ _ _ Null        _ = False
-search _ _ _ (Empty _ _) _ = False
-search p' graph targets start@(XO p _ _) visiting
-  | p /= p'                  = False
-  | start `S.member` targets = True
-  | otherwise = or [ search p' graph targets nb visiting' | nb <- nbs' ]
-  where
-    visiting' = S.insert start visiting
-    nbs       = MB.fromMaybe [] (M.lookup start graph)
-    nbs'      = filter (isPlayer p') . filter (`S.notMember` visiting') $ nbs
-
--- | Check if player `c` has connected top-to-bottom.
-won :: Player -> Board -> Bool
-won c board = or outcomes
-  where
-    hexs     = mkhexs board
-    graph    = hexagons Null hexs
-    starts   = filter (isPlayer c) . head $ hexs
-    targets  = S.fromList . filter (isPlayer c) . last $ hexs
-    outcomes = [ search c graph targets start S.empty | start <- starts ]
+neighbors :: a -> [[a]] -> [[[a]]]
+neighbors = undefined
 
 -- | Compute the Mark of the winner of a given board.
 winner :: [String] -> Maybe Mark
-winner board
-  | won X (L.transpose board') = Just Cross
-  | won O board'               = Just Nought
-  | otherwise                  = Nothing
-  where board' = map (filter (/= ' ')) board
+winner = undefined . map (filter (== ' ')) .  unstagger
