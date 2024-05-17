99e2e3635d70bd107caa5bfd65402ea6ea02ca5a
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-17 06:05:33 -0400

    Add better `RLE`

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -1,33 +1,17 @@
 module RunLength (decode, encode) where
 
-import qualified Data.Char as C
-import qualified Data.List as L
+import Data.Char
 
 decode :: String -> String
-decode "" = ""
-decode s@(c : cs)
-  | C.isDigit c =
-    let
-      count = read $ L.takeWhile (C.isDigit) s :: Int
-      rest = L.dropWhile C.isDigit s
-    in
-      case rest of
-        "" -> ""
-        (x : xs) -> (replicate count x) ++ decode xs
-  | otherwise = c : decode cs
+decode s =
+  case span isDigit s of
+    (_, "") -> ""
+    ("", c:cs) -> c : decode cs
+    (n, c:cs) -> replicate (read n :: Int) c ++ decode cs
 
 encode :: String -> String
 encode "" = ""
-encode cs = encodeWithCount cs 1
-
-
-encodeWithCount :: String -> Int -> String
-encodeWithCount "" _ = ""
-encodeWithCount (c : []) 1 = [c]
-encodeWithCount (c : []) n = show n ++ [c]
-encodeWithCount (a : b : bs) n
-  | a == b = encodeWithCount (b : bs) (n + 1)
-  | otherwise =
-    case n of
-      1 -> [a] ++ encodeWithCount (b : bs) 1
-      k -> show k ++ [a] ++ encodeWithCount (b : bs) 1
+encode (c:cs) = go $ span (== c) cs
+  where
+    go ("", suffix) = c : encode suffix
+    go (prefix, suffix) = show (1 + length prefix) ++ (c : encode suffix)
