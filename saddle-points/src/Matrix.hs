7b46645aa1b34bb4eaf93a4373a7197b214f8219
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-17 04:00:42 -0400

    Add someone else's better solution to `saddle-points` :)

--- saddle-points/src/Matrix.hs
+++ saddle-points/src/Matrix.hs
@@ -1,27 +1,13 @@
 module Matrix (saddlePoints) where
 
-import Data.Array (Array, (!))
-import qualified Data.Array as A
+import Data.Array
 
-saddlePoints :: Ord e => Array (Int, Int) e -> [(Int, Int)]
-saddlePoints matrix = filter (isSaddlePoint matrix) $ A.indices matrix
-
-isSaddlePoint :: Ord e => Array (Int, Int) e -> (Int, Int) -> Bool
-isSaddlePoint matrix i
-  | A.inRange bounds i = maxOfRow matrix i && minOfCol matrix i
-  | otherwise = False
-  where bounds = A.bounds matrix
-
-maxOfRow :: Ord e => Array (Int, Int) e -> (Int, Int) -> Bool
-maxOfRow matrix (x, y) = all (\i -> (matrix ! i) <= (matrix ! (x, y))) $ rowIxs matrix x
-
-rowIxs :: Ord e => Array (Int, Int) e -> Int -> [(Int, Int)]
-rowIxs matrix x = [(x, j) | j <- [a..b]]
-  where ((_, a), (_, b)) = A.bounds matrix
-
-minOfCol :: Ord e => Array (Int, Int) e -> (Int, Int) -> Bool
-minOfCol matrix (x, y) = all (\i -> (matrix ! i) >= (matrix ! (x, y))) $ colIxs matrix y
-
-colIxs :: Ord e => Array (Int, Int) e -> Int -> [(Int, Int)]
-colIxs matrix y = [(i, y) | i <- [a..b]]
-  where ((a, _), (b, _)) = A.bounds matrix
+saddlePoints :: Ord a => Array (Int, Int) a -> [(Int, Int)]
+saddlePoints a = (concatMap go . assocs) a
+  where
+    go ((x,y), u)
+      | null maxInRow && null minInCol = [(x,y)]
+      | otherwise = []
+      where 
+        maxInRow = [((i,j), v) | ((i,j), v) <- assocs a, i == x, v > u]
+        minInCol = [((i,j), v) | ((i,j), v) <- assocs a, j == y, v < u]
