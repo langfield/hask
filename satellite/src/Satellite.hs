b1d3b04af74734b15c1d590445f8a4f26ad76e58
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-26 18:35:23 +0200

    Golf a bit

--- satellite/src/Satellite.hs
+++ satellite/src/Satellite.hs
@@ -7,18 +7,14 @@ import BinaryTree (BinaryTree(..))
 
 treeFromTraversals :: Ord a => [a] -> [a] -> Maybe (BinaryTree a)
 treeFromTraversals [] _ = Nothing
-treeFromTraversals _ [] = Nothing
 treeFromTraversals [x] [y]
   | x == y = Just (Branch Leaf x Leaf)
   | otherwise = Nothing
 treeFromTraversals (x:xs) ys
-  | length (nubOrd ys) /= length ys = Nothing
-  | sort (x:xs) /= sort ys = Nothing
-  | otherwise = Branch <$> l <*> Just x <*> r
+  | sort (x:xs) /= sort (nubOrd ys) = Nothing
+  | otherwise = Branch <$> treeFromTraversals preL inL <*> Just x <*> treeFromTraversals preR inR
     where
       inL = takeWhile (/= x) ys
       inR = drop 1 $ dropWhile (/= x) ys
       preL = take (length inL) xs
       preR = drop (length inL) xs
-      l = treeFromTraversals preL inL
-      r = treeFromTraversals preR inR
