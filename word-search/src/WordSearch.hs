bddfc243a7ae5c81687b8ad6dee2fbad3362e2d5
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-30 17:38:00 +0200

    Working word search

--- word-search/src/WordSearch.hs
+++ word-search/src/WordSearch.hs
@@ -1,53 +1,55 @@
 module WordSearch (search, CharPos(..), WordPos(..)) where
 
-import Data.Bifunctor (bimap)
 import Data.List (findIndex, isPrefixOf, tails, transpose)
 import Data.Maybe (catMaybes, listToMaybe)
-import Data.Tuple (swap)
 
 import Debug.Trace (trace)
-
-data Dir = U | D | L | R | TLBR | TRBL | BLTR | BRTL deriving (Eq, Show)
-
 tt s x = trace (s ++ ": " ++ show x) x
-
-mkpos :: (Int,Int) -> CharPos
-mkpos (x,y) = CharPos (y+1) (x+1)
 
 data CharPos = CharPos {col :: Int, row :: Int} deriving (Eq, Show)
 data WordPos = WordPos {start :: CharPos, end :: CharPos} deriving (Eq, Show)
 
-trans :: ((Int,Int),(Int,Int)) -> ((Int,Int),(Int,Int))
-trans (a,b) = (swap a, swap b)
+rev :: WordPos -> WordPos
+rev (WordPos a b) = WordPos b a
 
-reflect :: Int -> ((Int,Int),(Int,Int)) -> ((Int,Int),(Int,Int))
-reflect m ((a,b),(c,d)) = ((m-a,b),(m-c,d))
+trans :: WordPos -> WordPos
+trans (WordPos (CharPos a b) (CharPos c d)) = WordPos (CharPos b a) (CharPos d c)
 
-shift :: Int -> (Int,Int) -> (Int,Int)
-shift n (x,y) | n > 0 = shift (n-1) (x+1,y+1)
-shift _ a = a
-
-undiag :: Int -> ((Int,Int),(Int,Int)) -> ((Int,Int),(Int,Int))
-undiag n (u,v) = (go u, go v)
+reflect :: [String] -> WordPos -> WordPos
+reflect xss (WordPos (CharPos a b) (CharPos c d)) = WordPos (CharPos a (m-b+1)) (CharPos c (m-d+1))
   where
-    go (x,y)
-      | x <= n = shift y (0,n-x)
-      | otherwise = shift y (x-n,0)
+    m = length xss
 
-mkloc :: String -> Int -> Int -> ((Int,Int),(Int,Int))
-mkloc w i j = ((i,j),(i,j + length w - 1))
+diagShift :: Int -> CharPos -> CharPos
+diagShift 0 c = c
+diagShift n (CharPos a b)
+  | n < 0 = CharPos a b
+  | otherwise = diagShift (n-1) (CharPos (a+1) (b+1))
 
-searchRow :: String -> Int -> String -> Maybe ((Int,Int),(Int,Int))
-searchRow w i = fmap (mkloc w i) . (findIndex (w `isPrefixOf`) . tails)
+undiag :: [String] -> CharPos -> CharPos
+undiag xss (CharPos a b)
+  | b <= n = diagShift (a-1) $ CharPos 1 (n-b+1)
+  | otherwise = diagShift (a-1) $ CharPos (b-n+1) 1
+  where
+    n = maybe 0 length (listToMaybe xss)
 
-searchWord :: String -> [String] -> [((Int,Int),(Int,Int))]
-searchWord w = catMaybes . zipWith (searchRow w) [0..]
+undiag' :: [String] -> WordPos -> WordPos
+undiag' xss (WordPos u v) = WordPos (undiag xss u) (undiag xss v)
 
-bisearch :: String -> [String] -> [((Int,Int),(Int,Int))]
-bisearch w xss = searchWord w xss ++ map swap (searchWord (reverse w) xss)
+mkpos :: String -> Int -> Int -> WordPos
+mkpos w i j = WordPos (CharPos (j+1) i) (CharPos (j + length w) i)
 
-quadsearch :: String -> [String] -> [((Int,Int),(Int,Int))]
-quadsearch w xss = bisearch w xss ++ map trans (bisearch w (transpose xss))
+searchRow :: String -> Int -> String -> Maybe WordPos
+searchRow w i = fmap (mkpos w i) . (findIndex (w `isPrefixOf`) . tails)
+
+searchWord :: [String] -> String -> [WordPos]
+searchWord xss w = catMaybes $ zipWith (searchRow w) [1..] xss
+
+bisearch :: [String] -> String -> [WordPos]
+bisearch xss w = searchWord xss w ++ map rev (searchWord xss (reverse w))
+
+quadsearch :: [String] -> String -> [WordPos]
+quadsearch xss w = bisearch xss w ++ map trans (bisearch (transpose xss) w)
 
 rot :: [String] -> [String]
 rot = transpose . reverse
@@ -58,15 +60,11 @@ diags xss = reverse top ++ bot
     top = transpose $ zipWith drop [0..] xss
     bot = transpose $ zipWith drop [1..] $ transpose xss
 
-diagsearch :: String -> [String] -> [((Int,Int),(Int,Int))]
-diagsearch w xss = map (trans . undiag n) (bisearch w (diags xss))
--- ++ map (reflect m . undiag n) (bisearch w (diags (rot xss)))
-  where
-    m = length xss
-    n = maybe 0 length (listToMaybe xss)
+diagsearch :: [String] -> String -> [WordPos]
+diagsearch xss w = map (trans . undiag' xss) (bisearch (diags xss) w) ++ map (reflect xss . undiag' xss) (bisearch (diags (rot xss)) w)
 
-octosearch :: String -> [String] -> [((Int,Int),(Int,Int))]
-octosearch w xss = quadsearch w xss ++ diagsearch w xss
+octosearch :: [String] -> String -> [WordPos]
+octosearch xss w = quadsearch xss w ++ diagsearch xss w
 
 search :: [String] -> [String] -> [(String, Maybe WordPos)]
-search grid = map (\w -> (w, fmap (uncurry WordPos . bimap mkpos mkpos) <$> listToMaybe $ octosearch w grid))
+search grid = map (\w -> (w, listToMaybe $ octosearch grid w))
